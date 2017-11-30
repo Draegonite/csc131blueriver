e6cd9fcf3f1567f9d62b0464103d32d9d6629155
Draegonite <34122875+Draegonite@users.noreply.github.com>
2017-11-30 02:45:26 -0800

    Update BudgetApplication.java

--- BudgetApplication.java
+++ BudgetApplication.java
@@ -6,15 +6,29 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableColumnModel;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 import javax.swing.JButton;
-import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import java.awt.Component;
+import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JTable;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JTextPane;
+import java.awt.Color;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
 
-public class BudgetApplication extends JFrame {
+public class BudgetApplication extends JFrame 
+{
 
 	private JPanel contentPane;
 	private JTable table;
@@ -22,13 +36,19 @@ public class BudgetApplication extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try
+				{
 					BudgetApplication frame = new BudgetApplication();
 					frame.setVisible(true);
-				} catch (Exception e) {
+				} 
+				catch (Exception e)
+				{
 					e.printStackTrace();
 				}
 			}
@@ -38,98 +58,86 @@ public class BudgetApplication extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public BudgetApplication() {
+	public BudgetApplication() 
+	{
+		Date date = new Date();
+		setTitle("BAP - Excel Spreadsheet " + date);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 719, 611);
+		setBounds(100, 100, 949, 606);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Upload Excel");
-		btnNewButton.setBounds(10, 90, 113, 23);
-		contentPane.add(btnNewButton);
+		JButton Upload = new JButton("Upload Excel");
+		Upload.setBounds(42, 165, 113, 23);
+		contentPane.add(Upload);
 		
-		JButton btnNewButton_1 = new JButton("Search");
-		btnNewButton_1.setBounds(10, 179, 113, 23);
-		contentPane.add(btnNewButton_1);
+		JButton search = new JButton("search");
+		search.setBounds(42, 254, 113, 23);
+		contentPane.add(search);
 		
-		JButton btnNewButton_2 = new JButton("Save");
-		btnNewButton_2.setBounds(10, 272, 113, 23);
-		contentPane.add(btnNewButton_2);
-
-		Object[] columns = {"Department", "CE", "CSC", "EEE", "CM", "ME", "ECSCS", "TOTAL"};
-		Object[][] data = new Object[0][0];
-		Object[] firstRowData = {"Department", "CE", "CSC", "EEE", "CM", "ME", "ECSCS", "TOTAL"};
-		Object[] secondRowData = {"Lab $", "10k", "20k", "30k", "20k", "20k", "0", "100k"};
-		Object[] thirdRowData = {"Lab %", "10%", "20%", "30%", "20%", "20%", "0%", "100%"};
-		Object[] fourthRowData = {"All $", "100k", "100k", "100k", "50k", "100k", "0", "450k"};
-		Object[] fifthRowData = {"All %", "22.2%", "22.2%", "22.2%", "11.1%", "22.2%", "0%", "100%"};
-		/*Object[][] rowData = {{"Department", "CE", "CSC", "EEE", "CM", "ME", "ECSCS", "TOTAL"}, 
-				{"Lab $", "10k", "20k", "30k", "20k", "20k", "0", "100k"},
-				{"Department", "10%", "20%", "30%", "20%", "20%", "0%", "100%"},
-				{"Department", "100k", "100k", "100k", "50k", "100k", "0", "450k"},
-				{"Department", "22.2%", "22.2%", "22.2%", "11.1%", "22.2%", "0%", "100%"}};*/
-		JTable table = new JTable();
-	    table.setModel(new DefaultTableModel(data,columns));
-		table.setBounds(155, 338, 463, 77);
-		contentPane.add(table);
-		
-		JButton btnNewButton_3 = new JButton("Generate Report");
-		btnNewButton_3.addActionListener(new ActionListener() 
-		{
-			public void actionPerformed(ActionEvent arg0) 
-			{
-				DefaultTableModel model = (DefaultTableModel) table.getModel();
-				model.addColumn(columns);
-				model.addRow(firstRowData);
-				model.addRow(secondRowData);
-				model.addRow(thirdRowData);
-				model.addRow(fourthRowData);
-				model.addRow(fifthRowData);
-				//JTable table = new JTable(rowData, columnNames);
-				//contentPane.add(table);
+		JButton save = new JButton("save");
+		save.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnNewButton_3.setBounds(10, 358, 113, 23);
-		contentPane.add(btnNewButton_3);
+		save.setBounds(42, 353, 113, 23);
+		contentPane.add(save);
+		
+		
+		JButton generateReport = new JButton("Generate Report");
+		generateReport.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				Report frame1 = new Report();
+				frame1.setVisible(true);
+			}
+		});
+		generateReport.setBounds(42, 454, 113, 23);
+		contentPane.add(generateReport);
+		
 		
 		JLabel lblExcelFile = new JLabel("Excel File:");
-		lblExcelFile.setBounds(155, 26, 56, 14);
+		lblExcelFile.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblExcelFile.setBounds(222, 50, 67, 23);
 		contentPane.add(lblExcelFile);
 		
-		JLabel lblLastEdited = new JLabel("Last Edited");
-		lblLastEdited.setBounds(279, 26, 72, 14);
+		JLabel lblLastEdited = new JLabel("Last Edited:");
+		lblLastEdited.setBounds(397, 54, 76, 14);
 		contentPane.add(lblLastEdited);
 		
-		JLabel lblNewLabel = new JLabel("Department2010.xlsx");
-		lblNewLabel.setBounds(155, 61, 114, 14);
+		JLabel lblDepartmentxlsx = new JLabel("Department2010.xlsx");
+		lblDepartmentxlsx.setBounds(222, 84, 136, 14);
+		contentPane.add(lblDepartmentxlsx);
+		
+		JLabel lblDepartmentxlsx_1 = new JLabel("Department2017.xlsx");
+		lblDepartmentxlsx_1.setBounds(222, 125, 136, 14);
+		contentPane.add(lblDepartmentxlsx_1);
+		
+		JLabel lblNewLabel = new JLabel("7/01/2010");
+		lblNewLabel.setBounds(397, 84, 76, 14);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Department2017.xlsx");
-		lblNewLabel_1.setBounds(155, 94, 114, 14);
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel label = new JLabel("7/01/2010");
-		label.setBounds(279, 61, 62, 14);
+		JLabel label = new JLabel("7/01/2017");
+		label.setBounds(397, 125, 76, 14);
 		contentPane.add(label);
 		
-		JLabel label_1 = new JLabel("7/01/2017");
-		label_1.setBounds(279, 94, 62, 14);
-		contentPane.add(label_1);
+		JLabel lblDepartmentxlsx_2 = new JLabel("Department 2017.xlsx");
+		lblDepartmentxlsx_2.setBounds(222, 357, 163, 14);
+		contentPane.add(lblDepartmentxlsx_2);
 		
 		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		panel.setBackground(Color.LIGHT_GRAY);
-		panel.setBounds(133, 11, 267, 107);
+		panel.setBounds(215, 341, 607, 47);
 		contentPane.add(panel);
 		
 		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		panel_1.setBackground(Color.LIGHT_GRAY);
-		panel_1.setBounds(155, 261, 197, 34);
+		panel_1.setBounds(215, 43, 305, 104);
 		contentPane.add(panel_1);
-		
-		JLabel lblDepartmentxlsx = new JLabel("Department 2017.xlsx");
-		panel_1.add(lblDepartmentxlsx);
-
 	}
 }
